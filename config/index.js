8af0d3e02397ca06acdd4ab55fe94e02b58e12a7
dongyuzhuo <1326628194@qq.com>
2018-12-06 15:50:35 +0800

    first commit

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
     },
     dev: {
         env: require('./dev.env'),
-        port: 5000,
+        port: 6000,
         autoOpenBrowser: false,
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
